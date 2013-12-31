3264502bf3d3187693af1c0a63fdc875d7478efa
Amy Ruan <nyc.amy@gmail.com>
2013-12-31 13:55:52 -0500

    changed config.assets.compile changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,10 +16,6 @@ role :db,  %w{deploy@example.com}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
-config.serve_static_assets = true
-config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-config.assets.compile = true
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
